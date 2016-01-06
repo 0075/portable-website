f060c28d2e6c40ee21d17c7173152aa52e31abec
075 <viktor.hagstrom@outlook.com>
2016-01-06 16:29:43 +0100

    added some meta content

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <!-- PHP -->
 <?php
 $top_margin = "5%";
-$title = "";
-$h1_text = "You know it!";
+$title = "Snail";
+
 ?>
 
 <html lang="en">
@@ -16,16 +16,16 @@ $h1_text = "You know it!";
   <title>
     <?php echo $title ?>
   </title>
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <meta name="description" content="hgstrm.se is a simple website where I can train my programming skills and make some fun stuff!">
+  <meta name="author" content="Viktor Hagström">
 
   <!-- Mobile Specific Metas -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Fonts -->
-  <link href='https://fonts.googleapis.com/css?family=Fira+Sans' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Droid+Sans+Mono' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Fira+Sans' rel='stylesheet' type='text/css'> <!-- Normal text -->
+  <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'> <!-- <h1> text -->
+  <link href='https://fonts.googleapis.com/css?family=Droid+Sans+Mono' rel='stylesheet' type='text/css'> <!-- Code -->
 
   <!-- CSS -->
   <link rel="stylesheet" href="css/normalize.css" type="text/css">
@@ -36,16 +36,22 @@ $h1_text = "You know it!";
 
 </head>
 
+<!-- Java Script -->
+<script>
+
+</script>
 
 
 <body>
 
   <!-- Primary Page Layout -->
+  <!-- –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
   <div class="container">
     <div class="row">
-      <div class="column" style="margin-top: <?php echo $top_margin ?>">
-        <h1>
-          <?php echo $h1_text ?>
+      <div class="column" style="margin-top: <?php echo $top_margin ?>" id="header">
+        <h1 id="title" style="center">
+          hgstrm
         </h1>
 
       </div>
